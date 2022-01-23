5140643f26e45ed8481beddf17b9971619f22bba
raja-chaudhary <raja@rajachaudhary.com>
2022-01-23 15:34:06 -0800

    Added HTMX to show "No Messages Yet" message

--- chat/urls.py
+++ chat/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
 ]
+
+htmx_urlpatterns = [
+    path('<str:room_name>/check-messages/',
+         views.check_messages, name='check-messages'),
+]
+
+urlpatterns += htmx_urlpatterns
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 from .models import Message
@@ -14,3 +16,16 @@ def room(request, room_name):
     room = Message.objects.filter(room=room_name)[0:50]
 
     return render(request, 'chat/room.html', {'room_name': room_name, 'username': username, 'messages': messages, 'room': room})
+
+
+def check_messages(request, room_name):
+    if request.method == 'GET':
+        message_exists = Message.objects.filter(
+            room=room_name).exists()
+    print(message_exists)
+    if message_exists:
+        return HttpResponse('<div id="no-messages" class="hide-this"></div>')
+    else:
+        return HttpResponse('''<div id="no-messages" style="min-height: 100px; max-height: 300px; overflow-y: scroll;">
+                            <p>No messages here!!</p>
+                        </div>''')
