f06001d435d866861682c6ebc25b09763d7c5059
raja-chaudhary <raja@rajachaudhary.com>
2022-01-24 06:43:29 -0800

    Updated url() to re_path() in routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,7 @@
-# from django.urls import path
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
