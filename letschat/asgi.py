06c03eb6aaa1838ef01823e9b5dc421f4a4cfdcc
raja-chaudhary <raja@rajachaudhary.com>
2022-01-23 21:07:45 -0800

    Added OriginValidators to asgi.py

--- letschat/asgi.py
+++ letschat/asgi.py
@@ -3,6 +3,7 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import OriginValidator
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letschat.settings')
@@ -13,9 +14,12 @@ from channels.auth import AuthMiddlewareStack
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    )
+    "websocket": OriginValidator(
+	AuthMiddlewareStack(
+        	URLRouter(
+            	chat.routing.websocket_urlpatterns
+        	)
+	),
+	[".herokuapp.com",],
+    ),
 })
