ba29b2cb485e6ef826ba79676d222b8771dde325
raja-chaudhary <raja@rajachaudhary.com>
2022-01-24 06:46:57 -0800

    removed other hosts from channel layers

--- letschat/settings.py
+++ letschat/settings.py
@@ -80,7 +80,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [("REDIS_TLS_URL", '127.0.0.1', 6379)],
+            'hosts': [("REDIS_TLS_URL",)],
         }
     }
 }
