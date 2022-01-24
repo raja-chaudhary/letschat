493aeda4f45a9b19366e34c5a725bf17a19288f7
raja-chaudhary <raja@rajachaudhary.com>
2022-01-23 18:22:59 -0800

    Initiated django setup in wsgi.py

--- letschat/wsgi.py
+++ letschat/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+import django
+django.setup()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letschat.settings')
 
